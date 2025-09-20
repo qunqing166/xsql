30d5499c3d5bb8363eaeefed2b8c11b21cf8ca50
qunqing166 <1730212106@qq.com>
2025-09-20 20:15:58 +0800

    .

--- include/SqlQuery.h
+++ include/SqlQuery.h
@@ -4,31 +4,38 @@
 #include <vector>
 #include <mysql/mysql.h>
 #include <QueryCondition.h>
-#include "xformat.h"
 #include "QueryResult.h"
 
 namespace x
 {
 
-class QueryBase
+class SelectQuery;
+class InsertQuery;
+class UpdateQuery;
+class DeleteQuery;
+
+
+class SqlQuery
 {
 public:
-    QueryBase(MYSQL* sql);
 
-    /**
-     * @brief 执行查询操作
-     * @return 查询结果对象
-     */
-    QueryResult Execute();
+    SqlQuery(MYSQL* sql);
 
-protected:
-    virtual void Generate() = 0;
+    SelectQuery Select(const std::string table, const std::vector<std::string>& fields);
+    InsertQuery Insert(const std::string& table, const std::vector<std::pair<std::string, std::string>>& fieldAndValue);
+    UpdateQuery Update(const std::string& table, const std::vector<std::pair<std::string, std::string>>& fieldAndValue);
+    DeleteQuery Delete(const std::string& table);
+   
+private:
 
     MYSQL* m_sql;
-    std::string m_query;
+
 };
 
-class SelectQuery: public QueryBase
+
+
+
+class SelectQuery
 {
 public:
     SelectQuery(MYSQL* sql, 
@@ -36,60 +43,27 @@ public:
                 const std::vector<std::string>& fields);
 
     SelectQuery& Where(const std::string& condition);
-    SelectQuery& Where(const QueryWhere& condition){
-        m_where = condition.Get();
-        return *this;
-    }
+    SelectQuery& Where(const QueryWhere& condition);
 
     SelectQuery& OrderBy(const std::string& field);
     SelectQuery& Limit(int count);
 
+    QueryResult Execute();
+
 protected:
 
-    virtual void Generate() override;
+    void Generate();
 
 private:
+
     std::string FormatFields(const std::vector<std::string>& fields);
-    // void GetTableFiledMeta(const std::string& table);
 
 private:
+    MYSQL* m_sql;
     std::string m_table;
     std::string m_select;
     std::string m_where;
     std::string m_oderby;
-};
-
-class SqlQuery
-{
-public:
-    SqlQuery(MYSQL* sql);
-
-    SelectQuery Select(const std::string table, const std::vector<std::string>& fields){
-        return SelectQuery(m_sql, table, fields);
-    }
-    SelectQuery Select(const std::map<std::string, std::vector<std::string>>& fields){
-        //return SelectQuery(m_sql, table, fields);
-    }
-    SelectQuery Select(const std::string table){
-        //return SelectQuery(m_sql, table);
-    }
-    void Insert();
-    void Update();
-    void Delete();
-   
-    void DisplayFields(){
-        for(auto& meta : m_metas){
-            std::cout << Format("table: {}\n", meta.first);
-            for(auto& it : meta.second.Fields()){
-                std::cout << Format("{}\n", it.first);
-            }
-        }
-    }
-
-
-private:
-    MYSQL* m_sql;
-    std::unordered_map<std::string, FieldMeta> m_metas;
 };
 
 
--- src/SqlQuery.cpp
+++ src/SqlQuery.cpp
@@ -6,28 +6,38 @@
 
 namespace x{
 
-QueryBase::QueryBase(MYSQL* sql):
+/**************************************************/
+
+SqlQuery::SqlQuery(MYSQL* sql):
     m_sql(sql)
 {
-    
+
 }
 
-QueryResult QueryBase::Execute()
-{
-    Generate();
-    if(mysql_query(m_sql, m_query.c_str())){
-        throw std::logic_error(Format("query error: {}", mysql_error(m_sql)));
-    }
-    return QueryResult(m_sql);
-}
 
 SelectQuery::SelectQuery(MYSQL* sql, 
             const std::string& table, 
             const std::vector<std::string>& fields):
-    QueryBase(sql),
+    m_sql(sql),
     m_table(table)
 {
     m_select = FormatFields(fields);
+}
+
+
+SelectQuery SqlQuery::Select(const std::string table, const std::vector<std::string>& fields)
+{
+    return SelectQuery(m_sql, table, fields);
+}
+
+
+/***************************************************/
+
+
+
+SelectQuery& SelectQuery::Where(const QueryWhere& condition){
+    m_where = condition.Get();
+    return *this;
 }
 
 std::string SelectQuery::FormatFields(const std::vector<std::string>& fields){
@@ -40,15 +50,33 @@ std::string SelectQuery::FormatFields(const std::vector<std::string>& fields){
     return ret;
 }
 
-SqlQuery::SqlQuery(MYSQL* sql):
-    m_sql(sql){
-}
 
-void SelectQuery::Generate(){
+QueryResult SelectQuery::Execute()
+{
     std::string sub_where;
     if(sub_where.empty() == false)sub_where = Format("where {}", m_where);
-    m_query = x::Format("select {} from {} {}", m_select, m_table, sub_where);
-    std::cout << m_query << '\n';
+    std::string query = x::Format("select {} from {} {}", m_select, m_table, sub_where);
+    std::cout << query << '\n';
+    if(mysql_query(m_sql, query.c_str())){
+        throw std::logic_error(Format("query error: {}", mysql_error(m_sql)));
+    }
+    return QueryResult(m_sql);
 }
+/******************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
